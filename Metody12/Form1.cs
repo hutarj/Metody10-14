269a35529d4c5c2a5ef4f8cbccfd215765fe3e7f
Jakub Hutař <hutarj.04@spst.cz>
2022-03-17 19:16:55 +0100

    Metody 12

--- Metody12/Form1.cs
+++ Metody12/Form1.cs
@@ -16,10 +16,38 @@ namespace Metody12
         {
             InitializeComponent();
         }
+        private bool ObsahujeSlovo(string s, out string nejdelsi, out string nejkratsi)
+        {
+            char[] oddelovac = { ' ' };
+            string[] slova = s.Split(oddelovac, StringSplitOptions.RemoveEmptyEntries);
+            nejkratsi = null;
+            nejdelsi = null;
+            bool obsahujeSlovo = slova.Length > 0;
+
+            if(obsahujeSlovo)
+            {
+                nejdelsi = slova[0];
+                nejkratsi = slova[0];
+
+                foreach (string item in slova)
+                {
+                    if (item.Length > nejdelsi.Length) nejdelsi = item;
+                    if (item.Length < nejkratsi.Length) nejkratsi = item;
+                }
+            }
+
+            return obsahujeSlovo;
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
             string s = textBox1.Text;
+            string nejdelsi, nejkratsi;
+            if (ObsahujeSlovo(s, out nejdelsi, out nejkratsi))
+            {
+                MessageBox.Show("Řetězec obsahuje alespoň jedno slovo\nNejdelší slovo - " + nejdelsi + "\nNejkratší slovo - " + nejkratsi);
+            }
+            else MessageBox.Show("Řetězec neobsahuje alespoň jedno slovo");
         }
     }
 }
