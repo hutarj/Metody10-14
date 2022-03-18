ec813eda5aad60224e00ed12410958bfde3ec1a2
Jakub Hutař <hutarj.04@spst.cz>
2022-03-18 07:07:42 +0100

    Metody 13

--- Metody13/Form1.cs
+++ Metody13/Form1.cs
@@ -16,5 +16,37 @@ namespace Metody13
         {
             InitializeComponent();
         }
+        private bool JeAlfnum(string s, out int pocetVel, out int pocetMal, out int pocetOst)
+        {
+            bool jeAlfnum = true;
+            pocetVel = 0;
+            pocetMal = 0;
+            pocetOst = 0;
+            foreach (char item in s)
+            {
+                if(char.IsLetterOrDigit(item) || char.IsWhiteSpace(item))
+                {
+                    if (char.IsUpper(item)) ++pocetVel;
+                    else if (char.IsLower(item)) ++pocetMal;
+                }
+                else
+                {
+                    jeAlfnum = false;
+                    ++pocetOst;
+                }
+            }
+            return jeAlfnum;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            int pocetMal;
+            int pocetVel;
+            int pocetOst;
+            if (JeAlfnum(s, out pocetVel, out pocetMal, out pocetOst)) MessageBox.Show("Řetězec je alfnumerický\n" + "Počet velkých písmen - " + pocetVel + "\nPočet malých písmen - " + pocetMal);
+            else MessageBox.Show("Řetězec není alfnumerický\n" + "Počet velkých písmen - " + pocetVel + "\nPočet malých písmen - " + pocetMal + "\nPočet ostatních znaků - " + pocetOst);
+
+        }
     }
 }
